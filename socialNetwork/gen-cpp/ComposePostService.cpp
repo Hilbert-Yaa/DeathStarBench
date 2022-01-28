80363cb44efeefb1838f2bc0856b1bd0afdbea01
hilbert_workstation <cyaa@umich.edu>
2022-01-28 04:33:01 -0500

    feat: add function wrapper

--- socialNetwork/gen-cpp/ComposePostService.cpp
+++ socialNetwork/gen-cpp/ComposePostService.cpp
@@ -5,9 +5,38 @@
  *  @generated
  */
 #include "ComposePostService.h"
+#include <fcntl.h>
+#include <iostream>
+#include <thrift/protocol/TDebugProtocol.h>
 #include <thrift/protocol/TProtocol.h>
+#include <thrift/protocol/TProtocolException.h>
+#include <thrift/protocol/TProtocolTap.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/transport/TFDTransport.h>
+#include <thrift/transport/TTransport.h>
+#include <thrift/transport/TTransportUtils.h>
 
 namespace social_network {
+
+void TRACE(const social_network::ComposePostService_ComposePost_args &args,
+           const char *fname = nullptr) {
+  using std::cout;
+  using std::endl;
+  using std::shared_ptr;
+  using namespace apache::thrift::protocol;
+  using namespace apache::thrift::transport;
+  std::cout
+      << "########################### Separator ###########################"
+      << std::endl;
+
+  TFDTransport *fdtptr;
+  int fd = open(fname, O_WRONLY);
+  fdtptr = new TFDTransport(fd == -1 ? STDOUT_FILENO : fd);
+  auto dump_prot = new TDebugProtocol(shared_ptr<TTransport>(
+      new TBufferedTransport(shared_ptr<TTransport>(fdtptr))));
+  args.write(dump_prot);
+  dump_prot->getTransport()->flush();
+}
 
 ComposePostService_ComposePost_args::
     ~ComposePostService_ComposePost_args() throw() {}
@@ -542,18 +571,9 @@ void ComposePostServiceProcessor::process_ComposePost(
 
   ComposePostService_ComposePost_args args;
   args.read(iprot);
-  // TODO: dump the protocols here
-  using std::shared_ptr;
-  using namespace apache::thrift::protocol;
-  using namespace apache::thrift::transport;
-  std::cout << "#### Test call" << std::endl;
-  auto dump_prot =
-      new TDebugProtocol(shared_ptr<TTransport>(new TBufferedTransport(
-          shared_ptr<TTransport>(new TFDTransport(STDOUT_FILENO)))));
-  args.write(dump_prot);
-  dump_prot->getTransport()->flush();
   iprot->readMessageEnd();
   uint32_t bytes = iprot->getTransport()->readEnd();
+  TRACE(args);
 
   if (this->eventHandler_.get() != NULL) {
     this->eventHandler_->postRead(ctx, "ComposePostService.ComposePost", bytes);
