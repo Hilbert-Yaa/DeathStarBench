634ef417e3e400e04aa44b350491585683677a00
hilbert_workstation <cyaa@umich.edu>
2022-02-10 03:28:07 -0500

    chore: remove stdout redirection

--- socialNetwork/scripts/run_compose_service.sh
+++ socialNetwork/scripts/run_compose_service.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-exec ComposePostService | tee /social-network-microservices/logs/trace.dat 2>&1
-# exec ComposePostService
+# exec ComposePostService | tee /social-network-microservices/logs/trace.dat 2>&1
+exec ComposePostService
