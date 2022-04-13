1824314189b4a90b4cda776ccb2416654ec454f7
hilbert_workstation <cyaa@umich.edu>
2022-04-13 17:59:30 -0400

    chore: remove dead code

--- socialNetwork/src/tracer/tracer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../../gen-cpp/ComposePostService.h"
-#include <thrift/protocol/TProtocol.h>
-
-namespace social_network {
-void TRACE(const social_network::ComposePostService_ComposePost_args &args,
-           const char *fname = nullptr);
-void DUMP(apache::thrift::protocol::TProtocol *iprot);
-} // namespace tracer
